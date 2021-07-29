46c0168570712a22f83b4faf38565305e6d18830
Tomi Lui <tomilui1221@gmail.com>
2021-07-28 21:36:51 -0700

    folder restructuring

--- index.js
+++ index.js
@@ -1,67 +1,10 @@
 const express = require("express")
 const app = express()
 const PORT = 6969
-const userData = require("./USER_DATA.json")
+const schema = require('./Schemas/index')
+
+// graphql imports
 const { graphqlHTTP } = require("express-graphql")
-const graphql = require('graphql')
-const {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList } = graphql
-
-
-
-const UserType = new GraphQLObjectType({
-
-    name:"User",
-    fields: () => ({
-        id: { type: GraphQLInt },
-        firstName: { type: GraphQLString },
-        lastName: { type: GraphQLString },
-        email: { type: GraphQLString },
-        password: { type: GraphQLString }
-    })
-})
-
-const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
-    fields: {
-        getAllUsers: {
-            type: new GraphQLList(UserType),
-            args: { id: {type: GraphQLInt} },
-            resolve(parent, args) {
-                return userData
-            } 
-        }
-    }
-})
-
-
-const Mutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-        createUser: {
-            type: UserType,
-            args: {
-                firstName: { type: GraphQLString },
-                lastName: { type: GraphQLString },
-                email: { type: GraphQLString },
-                password: { type: GraphQLString },
-            },
-            resolve( parent, args ) {
-                userData.push({
-                    id: userData.length + 1,
-                    firstName: args.firstName,
-                    lastName: args.lastName,
-                    email: args.email,
-                    password: args.password
-                })
-                // return feedback
-                return args
-            }
-        }
-    }
-})
-  
-
-const schema = new GraphQLSchema( {query: RootQuery, mutation: Mutation})
 
 
 // all parameters exist in the schema object
