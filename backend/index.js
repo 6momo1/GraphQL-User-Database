4c262bff40c6c1b3a7572a601abbbe3efea81f1a
Tomi Lui <tomilui1221@gmail.com>
2021-07-29 19:38:51 -0700

    client fetches username and email

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const app = express()
 const PORT = 6969
 const schema = require('./Schemas/index')
+var cors = require("cors")
 
 // graphql imports
 const { graphqlHTTP } = require("express-graphql")
 
+app.use(cors())
 
 // all parameters exist in the schema object
 app.use('/graphql', graphqlHTTP( {
